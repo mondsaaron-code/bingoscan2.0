0a00b732a707cb047a9bf98b67918c761127be3b
mondsaaron-code <monds.aaron@gmail.com>
2026-03-08 18:25:41 -0500

    Update orchestrator.ts

--- src/lib/orchestrator.ts
+++ src/lib/orchestrator.ts
@@ -15,6 +15,7 @@ import {
   getRecentReviewResolutionMemory,
   getRecentTitleOutcomeMemory,
   getScanById,
+  getEbaySearchPageCount,
   getScanResultMemory,
   getScpCacheCsvTextByStoragePath,
   getSellerOutcomeMemory,
